593b6f5514002631ae425b4834cb4aa338f2acf4
elsholz <hendrik.lankers.hl@googlemail.com>
2020-04-06 14:41:01 +0200

    add schema for json data

---
+++ backend/schema.py
@@ -0,0 +1,68 @@
+import jsonschema
+
+studentSchema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "rollnumber": {"type": "number"},
+        "marks": {"type": "number"},
+    },
+}
+rules_schema = {
+    "type": "object",
+    "properties": {
+        "shuffle_turn_order_on_start": {"type": "boolean"},
+        "enable_chat": {"type": "boolean"},
+        "finish_game_on_disconnect": {"type": "boolean"},
+        "finish_game_on_win": {"type": "boolean"},
+        "allow_reconnect": {"type": "boolean"},
+        "winning_row_length": {"type": "integer"},
+        "field_has_bounds": {"type": "boolean"},
+        "enable_cards": {"type": "boolean"},
+        "enable_cheats": {"type": "boolean"},
+        "number_of_players": {"type": "integer"},
+        "start_game_if_all_ready": {"type": "boolean"},
+        "variable_player_count": {"type": "boolean"},
+        "play_field_width": {"type": "integer"},
+        "play_field_height": {"type": "integer"},
+        "enable_gravity": {"type": "boolean"},
+        "game_is_public": {"type": "boolean"},
+        "card_placement_cooldown": {"type": "integer"},
+    }
+}
+card_deck_schema = {
+    "type": "object",
+    "properties": {
+        "ShuffleTurnOrder":{"type": "boolean"},
+        "ReverseTurnOrder":{"type": "boolean"},
+        "SkipNextTurn":{"type": "boolean"},
+    }
+}
+token_style_schema = {
+    "type": "object",
+    "properties": {
+        # TODO: Check if array of numbers
+        "color": {
+            "type": "array",
+            "items": {"type": "integer"}
+        }
+    }
+}
+player_schema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "token_style": token_style_schema
+    }
+}
+
+create_game_schema = {
+    "type": "object",
+    "properties": {
+        "game_name": {"type": "string"},
+        "rules": rules_schema,
+        "card_deck": card_deck_schema,
+        "player": player_schema,
+    }
+}
+
