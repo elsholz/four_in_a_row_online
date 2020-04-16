4b244d5814e643b077d676d7ba0aa1cc81ed2667
elsholz <hendrik.lankers.hl@googlemail.com>
2020-04-16 11:08:03 +0200

    add service

---
+++ four_in_a_row_online/tools/repo_updated.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+repo="four_in_a_row_online_backend"
+if [[ -d "$repo" ]]
+then
+	echo "Repo Exists, fetching…"
+	git --git-dir="/home/pi/$repo/.git" fetch 
+	lst="last_commit.txt"
+	touch $lst
+	current=$(git --git-dir="/home/pi/$repo/.git" show-branch master)
+	last=$(cat $lst)
+	echo "Current commit message: $current"
+	echo "Last commit message: $last"
+	if [ "$current" != "$last" ] 
+	then
+		echo "New commits found, pulling…"
+		echo $current > $lst
+		git --git-dir="/home/pi/$repo/.git" pull
+		exit 0
+	else
+		echo "No changes, not pulling"
+		exit 1
+	fi
+else
+	echo "Repo does not exist, cloning…"
+	git clone "https://github.com/elsholz/$repo.git"
+	exit 0
+fi
---
+++ four_in_a_row_online/tools/run_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do 
+	echo works
+	sleep 1m
+done
